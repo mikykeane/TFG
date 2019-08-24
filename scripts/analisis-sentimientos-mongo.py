fac65bb70e89e050460d4947a6d9cb9250d4aaa4
Miguel Keane <mikykeane@localhost.localdomain>
2019-08-24 06:54:57 +0200

    Analisis adaptado a python3.7 y creado YT2108

--- scripts/analisis-sentimientos-mongo.py
+++ scripts/analisis-sentimientos-mongo.py
@@ -19,8 +19,8 @@ import os.path
 
 # Conectamos MongoDB la base de datos "TwitterStream"
 connection = MongoClient('localhost', 27017)
-db = connection.TwitterNetflix2108
-db.tweets.ensure_index("id", unique=True, dropDups=True)
+db = connection.TwitterYT2108
+db.tweets.create_index("id", unique=True, dropDups=True)
 collection = db.tweets
 collection2 = db.concepts
 
@@ -28,14 +28,14 @@ collection2 = db.concepts
 
 url = "https://api.meaningcloud.com/sentiment-2.1"
 
-key= "85c3f8099b02079f164cdd3c4ad6a58e"
+key= "YOUR_KEY"
 lang="en"
 headers = {'content-type': 'application/x-www-form-urlencoded'}
 
 
 #Me aserguro que el archivo no exista previamente, para no escribir varias veces los titulos
-if not os.path.exists('data/Netflix2108.csv'):
-    with open('data/Netflix2108.csv', 'wb') as csvfile:
+if not os.path.exists('data/YT2108.csv'):
+    with open('data/YT2108.csv', 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',')
         filewriter.writerow(['Username', 'Followers', 'Confidence', 'Score_tag', 'Agreement', 'Subjectivity', 'Irony', 'Tweet'])
 
@@ -50,7 +50,7 @@ for cursor in db.tweets.find():
 
       print (username + ': ' + tweet)
       #payload= "key="+key+"&lang="+lang+"&txt="+txt
-      payload = "key=85c3f8099b02079f164cdd3c4ad6a58e&lang=en&of=json&txt= %s &txtf=plain&url=YOUR_URL_VALUE&doc=YOUR_DOC_VALUE" %(tweet)
+      payload = "key=YOURKEY&lang=en&of=json&txt= %s &txtf=plain&url=YOUR_URL_VALUE&doc=YOUR_DOC_VALUE" %(tweet)
       confidence= 0
       #Manejo excepciones por si la conexion da error que siga analizando la base de datos MongoDB
       try:
@@ -66,7 +66,7 @@ for cursor in db.tweets.find():
           sentimented_concept_list = answer['sentimented_concept_list']
           quoted_tweet = '"{}"'.format(tweet)
           #Me aseguro que la confianza en el analisis este en un rango aceptable
-          if confidence > 90:
+          if int(confidence) > 90:
               try:
                   concepts = {'id': id, 'score_tag':score_tag, 'entities': sentimented_entity_list, 'concepts':sentimented_concept_list, 'user':username , 'followers': followers, 'text': tweet}
                   collection2.save(concepts)
@@ -75,7 +75,7 @@ for cursor in db.tweets.find():
                     print("\n\n\n\n\n\nError al guardar en la base de datos MongoDB\n\n\n\n\n\n")
 
               #El parametro a es para "append", para actualizar el csv en vez de sobrescribirlo
-              with open('data/Netflix2108.csv', 'a') as csvfile:
+              with open('data/YT2108.csv', 'a') as csvfile:
                   filewriter = csv.writer(csvfile, delimiter=',')
                   filewriter.writerow([username, followers, confidence, score_tag, agreement, subjectivity, irony, quoted_tweet])
 
